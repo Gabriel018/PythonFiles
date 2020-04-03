4fa144602f446d123b61feafbcfead789cebb800
GabrielDuke <42377719+Gabriel018@users.noreply.github.com>
2020-04-03 16:04:27 -0300

    1.0

---
+++ Database.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import sqlite3
+
+sys = Tk()
+sys.title("DataBase")
+sys.iconbitmap("c:/PythonFiles/image/python.ico")
+sys.geometry("500x500")
+
+
+
+
+
+
+sys.mainloop()
---
+++ DropMenu.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+sys = Tk()
+sys.title("DropMenu")
+sys.iconbitmap("c:/PythonFiles/image/python.ico")
+sys.geometry("500x500")
+
+def mostrar():
+    M_Label = Label(sys, text=seleciona.get()).pack()
+
+Cidades = [
+        "Acrelândia",
+        "Assis Brasil",
+        "Brasiléia",
+        "Bujari",
+        "Capixaba",
+        "Cruzeiro do Sul",
+        "Epitaciolândia",
+        "Feijó"
+]
+seleciona = StringVar()
+seleciona.set(Cidades[0])
+Estados = OptionMenu(sys, seleciona,*Cidades)
+Estados.pack()
+
+btn = Button(sys, text="Mostrar", command=mostrar).pack()
+
+sys.mainloop()
